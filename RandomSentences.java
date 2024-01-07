6a311ce3771991f0ca0955bbc8f8c1d3ccf2fd6b
Tanya Stoyanova <145493604+tanyastoyanova87@users.noreply.github.com>
2024-01-07 16:53:20 +0200

    Added project "Random Sentences Generator"

---
+++ RandomSentences.java
@@ -0,0 +1,35 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RandomSentences {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        String[] names = {"Maria", "Ivan", "Georgi", "Alexander", "Silvia", "Teodor", "Mihaela", "Peter", "Radoslav", "Katrin"};
+        String[] places = {"Sandanski", "Varna", "Burgas", "Sofia", "Bansko", "Plovdiv", "Blagoevgrad", "Vraca", "Botevgrad", "Pernik"};
+        String[] verbs = {"walks", "plays with", "eats", "meets", "is running", "reads", "helps", "hears", "receives", "protects", "carries"};
+        String[] nouns = {"sandwich", "the dog", "his sister", "friends", "a book", "noise", "gifts", "her family", "groceries", "a magazine"};
+        String[] adverbs = {"quietly", "peacefully", "carefully", "slowly", "easily", "quickly", "usually", "rarely", "gently", "happily"};
+        String[] details = {"at home", "in the park", "in the forest", "at the mall", "near the river", "in the city"};
+
+        while (true) {
+            String randomName = getRandomWord(names);
+            String randomPlace = getRandomWord(places);
+            String randomVerb = getRandomWord(verbs);
+            String randomNoun = getRandomWord(nouns);
+            String randomAdverb = getRandomWord(adverbs);
+            String randomDetail = getRandomWord(details);
+
+            System.out.printf("%s from %s %s %s %s %s.%n", randomName, randomPlace, randomVerb, randomNoun, randomAdverb, randomDetail);
+            System.out.println("Click [Enter] to generate a new one.");
+        }
+    }
+
+    public static String getRandomWord (String[] words) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(words.length);
+        String word = words[randomIndex];
+
+        return word;
+    }
+}
